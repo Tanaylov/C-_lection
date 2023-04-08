8cfe88f06538543580a4cc6c96c40ad3292c7634
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-08 03:31:20 +0300

    First seminar

---
+++ Seminar1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter an integer: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+int result = userNumber * userNumber;
+double rer = 8/3;
+Console.Write($"{userInput}->{result}--{rer}");
+//Console.Write("->");
+//Console.Write(result);
