35f7af8162b8b79f056254044a7161b6d5cd4e95
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-09 16:28:34 +0300

    Exercises with array

---
+++ Lection2/Example003_Array1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {48, 55, 97, 111, 139, 45, 1, 8, 9, 139};
+
+int n = array.Length;
+int find = 139;
+
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.Write(index);
+    break;
+  }
+  index++;
+}
