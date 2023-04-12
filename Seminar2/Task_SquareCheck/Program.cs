dbf012abf67647aa55ce61feae3a3d94dcb396ab
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 19:03:37 +0300

    Task SquareCheck

---
+++ Seminar2/Task_SquareCheck/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, enter two integer: ");
+
+double userNumber1 = double.Parse(Console.ReadLine() ?? "");
+double userNumber2 = double.Parse(Console.ReadLine() ?? "");
+
+if (userNumber1 * userNumber1 == userNumber2 || userNumber2 * userNumber2 == userNumber1)
+{
+  Console.WriteLine($"{userNumber1}, {userNumber2} -> Yes");
+}
+else
+{
+  Console.WriteLine($"{userNumber1}, {userNumber2} -> No");
+}
