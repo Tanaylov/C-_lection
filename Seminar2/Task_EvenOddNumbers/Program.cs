ea8d49a2aaa61168d40c92fc5e1185c390ffddf2
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 17:58:34 +0300

    Added check for input number

--- Seminar2/Task_EvenOddNumbers/Program.cs
+++ Seminar2/Task_EvenOddNumbers/Program.cs
@@ -1,15 +1,36 @@
 ﻿Console.WriteLine("Enter two numbers: ");
 
-
+/*
 double userNumber1 = double.Parse(Console.ReadLine() ?? "");
 double userNumber2 = double.Parse(Console.ReadLine() ?? "");
+*/
+
+double userNumber1; 
+double userNumber2;
+
+while (true)
+{
+  //Console.Write("Enter two numbers: ");
+  if (double.TryParse(Console.ReadLine(), out userNumber1))
+    break;
+  Console.WriteLine("Incorrect enter first number");  
+}
+
+while (true)
+{
+  //Console.Write("Enter two numbers: ");
+  if (double.TryParse(Console.ReadLine(), out userNumber2))
+    break;
+  Console.WriteLine("Incorrect enter second number");
+}
+
 double remainder = userNumber1 % userNumber2;
 
-if (userNumber1 % userNumber2 != 0)
+if (remainder != 0)
 {
   Console.WriteLine($"{userNumber1}, {userNumber2} -> не кратно, остаток {remainder}");
 }
-else if (userNumber1 % userNumber2 == 0)
+else 
 {
   Console.WriteLine($"{userNumber1}, {userNumber2} -> кратно");
 }
