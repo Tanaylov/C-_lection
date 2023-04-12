cbcf8ef60104682fafc56917157d19fc48865232
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 19:12:50 +0300

    Task_RandomNumber  Code v.1.0

---
+++ Seminar2/Task_RandomNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int randomNumber = new Random().Next(10, 100);
+int digit1 = randomNumber / 10;
+int digit2 = randomNumber % 10;
+int max = digit1;
+
+if (digit2 > max)
+{
+  max = digit2;
+} 
+
+Console.Write($"{randomNumber} -> {max}");
