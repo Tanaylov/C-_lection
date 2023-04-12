50ecad6bb99795cd397f7c4e17a5dc2b91d0e4b5
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 22:30:42 +0300

    Task 15 - done, Task 13 - No

---
+++ Homework/HW2/Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+Console.Clear();
+
+double numberOfDay;
+
+while (true)
+{
+  Console.WriteLine("Enter the number that matches the day of the week: ");
+  if(double.TryParse(Console.ReadLine(), out numberOfDay))
+  break;
+  Console.WriteLine("was not a number entered");
+}
+
+if (numberOfDay > 0 && numberOfDay < 6)
+{
+  Console.WriteLine($"{numberOfDay} -> No");
+}
+else if (numberOfDay == 6 || numberOfDay == 7)
+{
+  Console.WriteLine($"{numberOfDay} -> Yes");
+}
+else
+{
+  Console.WriteLine($"The number isn't matches the day of the week.");
+}
