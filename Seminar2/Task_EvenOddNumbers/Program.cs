27bc4266bf9d3988c74145e89000612b4fdf7580
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 17:17:06 +0300

    Working code_2

--- Seminar2/Task_EvenOddNumbers/Program.cs
+++ Seminar2/Task_EvenOddNumbers/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter two numbers: ");
+
+
+double userNumber1 = double.Parse(Console.ReadLine() ?? "");
+double userNumber2 = double.Parse(Console.ReadLine() ?? "");
+double remainder = userNumber1 % userNumber2;
+
+if (userNumber1 % userNumber2 != 0)
+{
+  Console.WriteLine($"{userNumber1}, {userNumber2} -> не кратно, остаток {remainder}");
+}
+else if (userNumber1 % userNumber2 == 0)
+{
+  Console.WriteLine($"{userNumber1}, {userNumber2} -> кратно");
+}
