8ba20afccc2e269ec0034095ceeb31b9c8562a28
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 23:14:41 +0300

    finished Task13

--- Homework/HW2/Task13/Program.cs
+++ Homework/HW2/Task13/Program.cs
@@ -4,4 +4,29 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+//int userNumber = new Random().Next(1000000);
+
+double userNumber;
+
+while (true)
+{
+  Console.WriteLine("Please, enter a number: ");
+  if (double.TryParse(Console.ReadLine(), out userNumber))
+    break;
+  Console.WriteLine("Incorrect enter number");
+}
+
+if (userNumber % 100 == userNumber)
+{
+  Console.WriteLine($"{userNumber} -> there is no third digit");
+}
+else
+{
+  int thirdDigit = int.Parse(Convert.ToString(userNumber)[2].ToString());
+  Console.WriteLine($"{userNumber} -> {thirdDigit}");
+}
+
+//int a = int.Parse(Convert.ToString(x)[1].ToString());
