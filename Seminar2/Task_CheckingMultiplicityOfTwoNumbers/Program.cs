7a2d8661a099e8617d3f09968ce65a46afbd813e
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 18:21:46 +0300

    Added string Console.Clear()

---
+++ Seminar2/Task_CheckingMultiplicityOfTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+double userNumber;
+
+while (true)
+{
+  Console.WriteLine("Please, enter a number: ");
+  if (double.TryParse(Console.ReadLine(), out userNumber))
+    break;
+  Console.WriteLine("Incorrect enter first number");  
+}
+
+if (userNumber % 7 == 0 && userNumber % 23 == 0)
+{
+  Console.WriteLine("Yes");
+}
+else
+{
+  Console.WriteLine("No");
+}
