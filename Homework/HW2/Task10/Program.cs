e65abef08c8f12ad4e9b73f8f72df741d8904db7
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-12 20:00:08 +0300

    HW2 Task10

---
+++ Homework/HW2/Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int userNumber;
+
+while (true)
+{
+  Console.WriteLine("Please, enter a three-digit number: ");
+  if (int.TryParse(Console.ReadLine(), out userNumber))
+    break;
+  Console.WriteLine("Incorrect enter a number");  
+}
+
+int digit2 = userNumber / 10 % 10;
+string userNumberS = userNumber.ToString();
+int len = userNumberS.Length;
+
+if (len == 3)
+{
+  Console.WriteLine($"{userNumber} -> {digit2}");
+}
+else
+{
+  Console.WriteLine("Need enter 3-digit number");
+}
