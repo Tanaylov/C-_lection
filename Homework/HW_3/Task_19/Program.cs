4c6eb7ef3a52814758bf4946593ae9e7a66536ba
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-15 16:23:56 +0300

    first task after third seminar

---
+++ Homework/HW_3/Task_19/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+int userNumber;
+
+while (true)
+{
+  Console.WriteLine("Enter 5-digits number: ");
+
+  if (int.TryParse(Console.ReadLine(), out userNumber) && userNumber.ToString().Length == 5)
+    break;
+  Console.WriteLine("What you entered is not a number or not 5-digits number, please, try again");
+}
+
+int digit_1 = userNumber / 10000;
+int digit_2 = userNumber / 1000 % 10;
+int digit_4 = userNumber % 100 / 10;
+int digit_5 = userNumber % 10;
+
+if (digit_1 != digit_5 || digit_2 != digit_4)
+{
+  Console.WriteLine(userNumber + "-> No");
+}
+else 
+{
+  Console.WriteLine(userNumber + "-> Yes");
+}
