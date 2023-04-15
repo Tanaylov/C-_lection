1a840ec2720bcd7a26408e8e298a8c21e67c0d48
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-15 23:58:29 +0300

    Third task after third seminar

---
+++ Homework/HW_3/Task_23/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+
+Console.Clear();
+
+int userNum;
+
+while(true)
+{
+  Console.WriteLine("Please, enter a whole number from 1 to N: ");
+  if(int.TryParse(Console.ReadLine(), out userNum) && userNum >= 1)
+  break;
+  Console.WriteLine("You entered not a number or not correct number");
+}
+
+int index;
+int[] cube = new int[userNum + 1];
+
+Console.Write($"{userNum} -> ");
+
+for (index = 1; index < userNum + 1; index ++)
+{
+  cube [index] = index*index*index;
+  Console.Write(cube[index].ToString() + ", ");
+  if (index == userNum)
+  {
+    Console.Write(cube[index].ToString());
+  }
+  
+} 
