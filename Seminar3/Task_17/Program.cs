b283c56404c0f4791104e299814ef9076262be61
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-16 18:51:51 +0300

    Modified code in Task17

--- Seminar3/Task_17/Program.cs
+++ Seminar3/Task_17/Program.cs
@@ -1,8 +1,27 @@
 ﻿Console.Clear();
 
-double valueX;
-double valueY;
+double valueX = 0, valueY = 0;
 
+static void checkingOnValid (ref double userInPut, string axis)
+{
+  while(true)
+  {
+    try
+    {
+      Console.Write($"Enter {axis}-value: ");
+      userInPut = double.Parse(Console.ReadLine().Replace('.',',') ?? "");
+      break;
+    }
+    catch(Exception exc)
+    {
+      Console.WriteLine($"Entered an invalid value. {exc.Message}");
+    }
+  }
+}
+checkingOnValid(ref valueX, "X");
+checkingOnValid(ref valueY, "Y");
+
+/*
 while(true)
 {
   Console.Write("Enter X-axis value: ");
@@ -18,35 +37,45 @@ while(true)
   break;
   Console.WriteLine("You entered an invalid value.");
 }
-
-//Console.Write("Enter X-axis value: ");
-//double valueX = double.Parse(Console.ReadLine().Replace('.',',') ?? "");
-
-//Console.Write("Enter Y-axis value: ");
-//double valueY = double.Parse(Console.ReadLine().Replace('.',',') ?? "");
-
-//Console.Write(valueX + valueY);
-
-if (valueX > 0 && valueY > 0)
-{
-  Console.WriteLine("First quarter");
-}
-else if (valueX < 0 && valueY > 0)
-{
-  Console.WriteLine("Second quarter");
-}
-else if (valueX < 0 && valueY < 0)
-{
-  Console.WriteLine("Third quarter");
-}
-else if (valueX > 0 && valueY < 0)
-{
-  Console.WriteLine("Fourth quarter");
-}
+*/
 
 /*
-else
+try
 {
-  Console.WriteLine("одна из координат лежит на оси");
+  Console.Write("Enter X-axis value: ");
+  valueX = double.Parse(Console.ReadLine().Replace('.',',') ?? "");
+
+  Console.Write("Enter Y-axis value: ");
+  valueY = double.Parse(Console.ReadLine().Replace('.',',') ?? "");
+}
+catch(Exception exc)
+{
+  Console.Write($"Entered an invalid value {exc.Message}");
+  return;
 }
 */
+static void PrintQuaterOfCoordinates (double valueX, double valueY)
+{
+  if (valueX > 0 && valueY > 0)
+  {
+    Console.WriteLine("First quarter");
+  }
+  else if (valueX < 0 && valueY > 0)
+  {
+    Console.WriteLine("Second quarter");
+  }
+  else if (valueX < 0 && valueY < 0)
+  {
+    Console.WriteLine("Third quarter");
+  }
+  else if (valueX > 0 && valueY < 0)
+  {
+    Console.WriteLine("Fourth quarter");
+  }
+  else
+  {
+    Console.WriteLine("One of the coordinates lies on the axis");
+  }
+}
+
+PrintQuaterOfCoordinates (valueX, valueY);
