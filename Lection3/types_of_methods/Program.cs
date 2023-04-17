3bbca6a40296e00038ca7e80731ed8b00377ead4
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-17 17:04:40 +0300

    studied the types of  methods

---
+++ Lection3/types_of_methods/Program.cs
@@ -0,0 +1,68 @@
+﻿// 1 Type - ничего не принимает на вход и ничего не возвращает.
+
+void Method1()
+{
+  Console.Clear();
+  Console.WriteLine("Hello, Ivan!");
+}
+
+Method1 ();
+
+
+//2 Type - принимает на вход переменные, но ничего не возвращает.
+
+void Method2(string name)
+{
+  //Console.Clear();
+  Console.WriteLine($"Hello, {name}!");
+}
+
+Method2 ("Anna");
+
+void Method2_1(string name, int c)
+{
+  int i = 0;
+  while(i < c)
+  {
+    Console.WriteLine($"Hello, {name}! - {c}");
+    i ++;
+  }
+}
+
+Method2_1("Annna", 2);
+Method2_1 (c: 3, name: "Anna");
+
+
+//3 Type - ничего не принимает на вход, но возвращает значение.
+
+int Method3()
+{
+  return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+
+//4 Type - И принимает на вход значение и возвращает какие-то значениея.
+
+string Method4 (int count, string text)
+// {
+//   int index = 0;
+//   string result = String.Empty;
+//   while (index < count)
+//   {
+//     result += text;
+//     index ++;
+//   }
+//   return result;
+// }
+{
+  string result = String.Empty;
+  for (int index = 0; index < count; index ++)
+    result += text;
+  return result;
+}
+
+string res = Method4(3, "Hi++-");
+Console.WriteLine(res);
