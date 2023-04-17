94d063e9530c459a353120c781542e1c084aee69
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-17 20:11:15 +0300

    лекция 3

---
+++ Lection3/Cycle_for/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+/*
+for (int i = 1; i <= 10; i ++)
+{
+  for (int j = 1; j <= 10; j ++)
+  {
+    Console.WriteLine($"{i} x {j} = {i * j}");
+  }
+  Console.WriteLine();
+}
+*/
+
+string Text(string text1)
+{
+  string text2 = text1.Replace(' ','_').Replace('П','п');
+  return text2;
+}
+
+string textM = Text("Привет - это я.");
+Console.Write(textM);
