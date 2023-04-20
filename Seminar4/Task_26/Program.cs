54a7b4ce6c40faec65ef6d89601ef1633d4eda2d
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-20 16:26:52 +0300

    First Task on Seminar4

---
+++ Seminar4/Task_26/Program.cs
@@ -0,0 +1,34 @@
+﻿// 489 -> 3; 78945 -> 5
+Console.Clear();
+
+int userNumber = InputFromUser("Enter whole number/integer: ", "You entering not a number or not correct number");
+int count = DigitCount(userNumber);
+OutputResult(userNumber, count);
+
+int InputFromUser(string messegeForUser, string errorMessage)
+{
+  while (true)
+  {
+    Console.WriteLine(messegeForUser);
+    if (int.TryParse(Console.ReadLine(), out int userNumber))
+    return userNumber;
+    Console.WriteLine(errorMessage);
+  }
+  string numberFromUser = userNumber.ToString();
+} 
+int DigitCount(int num)
+{
+  string numberFromUser = num.ToString();
+  int result = 0;
+  int index = numberFromUser.Length;
+  while(index > 0)
+  {
+    result += 1;
+    index--;
+  }
+  return result;
+}
+void OutputResult(int InputNumber, int digitsCount)
+{
+  Console.WriteLine($"{InputNumber} -> {digitsCount}");
+}
