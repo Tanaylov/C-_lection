5414277aa514fcc5e8e3d48359eb54ea8415cceb
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-20 23:21:26 +0300

    Task_29 HW_4

---
+++ Homework/HW_4/Task_29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+Console.Clear();
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+  while (index < length)
+  {
+    collection[index] = new Random().Next(1, 100);
+    index++;
+  }
+}
+
+void PrintArray(int[] col)
+{
+  int count = col.Length;
+  int position = 0;
+  Console.Write("[");
+  while (position < count - 1)
+  {
+    Console.Write(col[position] + ", ");
+    position++;
+  }
+  Console.Write(col[count - 1] + "]");
+}
+
