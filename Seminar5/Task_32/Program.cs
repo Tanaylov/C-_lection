238fa38cfc45d33240ddf8abcc1e2e05be393c5d
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-24 00:37:46 +0300

    task_32/33 in seminar5

---
+++ Seminar5/Task_32/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+byte len = Convert.ToByte(new Random().Next(1, 10));
+double st = new Random().NextDouble() * (-1 + 5) - 5,
+       fin = new Random().NextDouble() * (15 - 3) + 3;
+
+double [] myArray = GetRandomArray(len, st, fin);
+
+System.Console.WriteLine(String.Join(" // ", myArray));
+
+ArrayReverse(myArray);
+
+double [] GetRandomArray(byte length, double start, double finish)
+{
+  double [] array = new double[length];
+  for(byte i = 0; i < length; i++) 
+  {
+    array[i] = new Random().NextDouble() * (finish - start) + start;
+    array[i] = Math.Round(array[i], 4);
+  }
+  return array;
+}
+void ArrayReverse(double [] array1)
+{
+  for(byte j = 0; j < array1.Length; j++)
+  {
+    array1[j] = array1[j] / -1;
+    System.Console.WriteLine(array1[j] + "||");
+  }
+}
+
+/*
+if(array1[i] > 0) array1[i] *= -1; 
+    else array1[i] = Math.Abs(array1[i]);
+*/    
---
+++ Seminar5/Task_33/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+int len = 0, start = 0, finish = 0, num = 0;
+int lenAr = GetDateFromUser(ref len, "length");
+int startArr = GetDateFromUser(ref start, "start");
+int finishArr = GetDateFromUser(ref finish, "finish");
+int searchingNum = GetDateFromUser(ref num, "number");
+
+int [] arrayS = GetArray(lenAr, startArr, finishArr);
+
+System.Console.WriteLine(String.Join(" ** ", arrayS));
+CheckingForNumber(arrayS, searchingNum);
+
+int [] GetArray(int lenAr, int startArr, int finishArr)
+{
+  int [] array = new int[lenAr];
+  for(byte i = 0; i < lenAr; i++)
+    array[i] = new Random().Next(startArr, finishArr);
+  return array;  
+}
+
+int GetDateFromUser(ref int dates, string date)
+{
+ System.Console.WriteLine($"Enter {date}");
+ dates = Convert.ToInt32(Console.ReadLine());
+ return dates;
+}
+
+void CheckingForNumber(int [] array, int number)
+{
+  int count = 0;
+  foreach(int el in array)
+  {
+   if(el == number) count++;
+  }
+  if(count != 0) System.Console.WriteLine($"Yes, {count} times");
+  else System.Console.WriteLine("NO");
+}
