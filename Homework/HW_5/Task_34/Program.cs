98bcba1575a6137decae8ba5057102bad915c3d5
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-23 15:26:42 +0300

    done first task after 5 eminar

---
+++ Homework/HW_5/Task_34/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2.
+Console.Clear();
+
+int [] array3dig = CreatThreeDigitsArray();
+PrintArray(array3dig);
+ushort countEvenNumber = CountEvenNumberInArray (array3dig);
+PrintResult(array3dig, countEvenNumber);
+
+
+void PrintResult (int[] array, ushort count)
+{
+  Console.WriteLine("[" + String.Join(" | ", array) + "]. " + "Count of even numbers equal " + count);
+}
+
+void PrintArray(int[] array)
+{
+  Console.WriteLine("[" + String.Join(" | ", array) + "]");
+}
+
+int[] CreatThreeDigitsArray()
+{
+  int lenAr = new Random().Next(3, 10);
+  int [] array = new int[lenAr];
+  for(byte i = 0; i < lenAr; i++) array[i] = new Random().Next(100, 1000);
+  return array;
+}
+
+ushort CountEvenNumberInArray(int[] array)
+{
+  ushort count = 0;
+  for(byte i = 0; i < array.Length; i++) 
+  {
+    if(array[i] % 2 == 0) count++;
+  }
+  return count;
+}
