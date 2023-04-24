ec694e2b29ce80e8ebb8c824c1ecd8fa9ea0ef9a
Ivan Tanajlov <wasserleu@gmail.com>
2023-04-24 23:28:35 +0300

    Task_35&37 Seminor5

---
+++ Seminar5/Task_35/Program.cs
@@ -0,0 +1,27 @@
+﻿// 10 - 99 cout index
+Console.Clear();
+
+int [] array = GetArray (50);
+System.Console.WriteLine(string.Join("*", array));
+
+byte count = CountIndex(array, 10, 20);
+System.Console.WriteLine($"Count elements on 10 to 99 equal {count}");
+
+
+
+int [] GetArray(int num)
+{
+  int [] arr = new int[num];
+  for(byte i = 0; i < num; i++)
+    arr[i] = new Random().Next(1, num + 1);
+  return arr;
+}
+
+byte CountIndex( int [] array, int numS, int numF)
+{
+  byte count = 0;
+  foreach(byte el in array)
+    if(el >= numS && el <= numF)
+      count++;
+  return count;
+}
---
+++ Seminar5/Task_37/Program.cs
@@ -0,0 +1,79 @@
+﻿Console.Clear();
+
+double [] array = GetArray();
+System.Console.WriteLine("[" + string.Join(" / ", array) + "]");
+array = ArraySumElements(array);
+System.Console.WriteLine("[" + string.Join(" | ", array) + "]");
+
+double [] GetArray()
+{
+  byte len;
+  double finish, start;
+  while(true)
+  {
+    System.Console.WriteLine("Set the length of the array: ");
+    if(byte.TryParse(Console.ReadLine(), out len))
+      break;
+    System.Console.WriteLine("Wrong enter!");
+  }
+
+  while(true)
+  {
+    System.Console.WriteLine("Set the beginning of the array: ");
+    if(double.TryParse(Console.ReadLine().Replace('.',','), out start))
+      break;
+    System.Console.WriteLine("Wrong enter!");
+  }
+
+  while(true)
+  {
+    System.Console.WriteLine("Set the end of the array: ");
+    if(double.TryParse(Console.ReadLine().Replace('.',','), out finish))
+      break;
+    System.Console.WriteLine("Wrong enter!");
+  }
+
+  double [] arr = new double[len];
+  for(byte i = 0; i < len; i++)
+  {
+    arr[i] = new Random().NextDouble() * (finish - start) + start;
+    arr[i] = Math.Round(arr[i], 3);
+  }
+  return arr;
+}
+
+double [] ArraySumElements(double [] arrOrigine)
+{
+  byte size = Convert.ToByte(arrOrigine.Length);
+  if(size % 2 == 0)
+  {
+    double [] newArray = new double[size / 2];
+    for(byte i = 0; i < size; i++)
+  {
+      newArray[i] = arrOrigine[i] * arrOrigine[size-1];
+      newArray[i] = Math.Round(newArray[i], 3);
+      size--;
+  }
+    return newArray;
+  }
+  else
+  {
+    byte newSize = Convert.ToByte(size / 2 + 1);
+    double [] newArray = new double[newSize];
+    for(byte i = 0; i < size; i++)
+    {
+      if(newSize < size)
+      {
+      newArray[i] = arrOrigine[i] * arrOrigine[size-1];
+      newArray[i] = Math.Round(newArray[i], 3);
+      }
+      else
+      {
+      newArray[i] = arrOrigine[newSize - 1];
+      newArray[i] = Math.Round(newArray[i], 3);
+      }
+      size--;
+    }
+    return newArray;
+  }
+}
