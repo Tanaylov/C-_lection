33bd6073bd3c6f3cfbea1a0883bf030cc85782be
Ivan Tanajlov <wasserleu@gmail.com>
2023-05-03 00:36:45 +0300

    task_44 & 45 in 6 seminar

---
+++ Seminar6/Task_44/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+byte number = GetNumberForFibonacciSequence();
+short[] numberFibonacci = GetFibonacciNumber(number);
+System.Console.WriteLine(number + " ==> " + String.Join(", ", numberFibonacci));
+
+byte GetNumberForFibonacciSequence()
+{
+  byte num;
+  while(true)
+  {
+    System.Console.WriteLine("Enter a number: ");
+    if(byte.TryParse(Console.ReadLine() ?? "", out num) && num > 0)
+      break;
+    System.Console.WriteLine("You enter uncorrect number, try again, please");
+  }
+  return num;
+}
+
+
+short[] GetFibonacciNumber(byte num)
+{
+  short[] array = new short[num];
+  array[0] = 0;
+  if(num == 1) return array;
+  array[1] = 1;
+  if(num == 2) return array;
+  else
+  {
+    for(byte i = 2; i < number; i++)
+      array[i] = (short)(array[i - 1] + array[i - 2]);
+    return array;
+  }
+}
---
+++ Seminar6/Task_45/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+double[] arr = new double[new Random().Next(1, 9)];
+
+for(byte i = 0; i < arr.Length; i++)
+  arr[i] = Math.Round(new Random().NextDouble() * (11 + 11) - 11, 2);
+System.Console.WriteLine(String.Join("/", arr));
+
+System.Console.WriteLine("-------------------------------------------");
+
+double[] arrCopy = new double[arr.Length];
+for(byte j = 0; j < arr.Length; j++)
+  arrCopy[j] = arr[j];
+System.Console.WriteLine(String.Join("/", arrCopy));
