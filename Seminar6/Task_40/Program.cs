92a1623c93884dc26648a799895b713af9b29f60
Ivan Tanajlov <wasserleu@gmail.com>
2023-05-02 22:36:45 +0300

    task_40 and task_42 after 6 seminar

---
+++ Seminar6/Task_40/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+double num1 = double.Parse(Console.ReadLine() ?? "");
+double num2 = double.Parse(Console.ReadLine() ?? "");
+double num3 = double.Parse(Console.ReadLine() ?? "");
+bool result = TriangleCheck(num1, num2, num3);
+System.Console.WriteLine(result);
+
+bool TriangleCheck(double n1, double n2, double n3)
+{
+  if(n1 + n2 > n3 && n1 + n3 > n2 && n2 + n3 > n1) 
+    return true;
+  else
+    return false;
+}
---
+++ Seminar6/Task_42/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+
+int number = int.Parse(Console.ReadLine() ?? "");
+
+byte len = LengthDetectionForBinaryNum(number);
+byte[] binaryNum = new byte[len];
+int number1 = number;
+
+byte LengthDetectionForBinaryNum(int num)
+{
+  if(num < 2) return 1;
+  byte len = 1;
+  int index = 2;
+  for(byte i = 2; i < 100; i++)
+    {
+      int ind = index * 2;
+      byte len1 = Convert.ToByte(len * i);
+      if(num >= index && num < ind) return len1;
+      index *= 2;
+    }
+  if(num > index) return 0;
+  else return 0;
+}
+
+
+for(byte i = 0; i < len; i++)
+{
+  if(number1 % 2 != 0)  binaryNum[len - i - 1] = 1;
+  else binaryNum[len - i - 1] = 0;
+  number1 /= 2;
+}  
+
+//int[] array = { 5, 6, 2, 4 };
+int finalScore = 0;
+for (int i = 0; i < len; i++)
+{
+    finalScore += binaryNum[i] * Convert.ToInt32(Math.Pow(10, len-i-1));
+}
+System.Console.WriteLine(number + " = " + String.Join("", binaryNum) + "//" +  finalScore);
