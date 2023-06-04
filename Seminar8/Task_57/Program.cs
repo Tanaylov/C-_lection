b4557d9a7e3ed2de7b97a5ce1c4ed1270121bfa4
Ivan Tanajlov <wasserleu@gmail.com>
2023-06-04 15:41:52 +0300

    finished Task_57 and Task_59 in progress

---
+++ Seminar8/Task_57/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Clear();
+
+byte[,] array2D = new byte[4, 4];
+
+for(var i = 0; i < array2D.GetLength(0); i++)
+  for(var j = 0; j < array2D.GetLength(1); j++)
+    array2D[i, j] = (byte)new Random().Next(10, 20);
+
+for(var i = 0; i < array2D.GetLength(0); i++)
+  {
+    for(var j = 0; j < array2D.GetLength(1); j++)
+      System.Console.Write(array2D[i, j] + " \\ ");
+    System.Console.WriteLine();
+  }  
+string[] match = new string[array2D.GetLength(0) * array2D.GetLength(1)];
+byte b = 0;
+for(var i = 0; i < array2D.GetLength(0); i++)
+  for(var j = 0; j < array2D.GetLength(1); j++)
+    {
+      byte a = 0;
+      for(var k = 0; k < array2D.GetLength(0); k++)
+        for(var n = 0; n < array2D.GetLength(1); n++)
+          if(array2D[k, n] == array2D[i, j]) a++; 
+      match[b] = array2D[i, j] + " exist " + a + " count";
+      b++;
+    }
+
+System.Console.WriteLine(String.Join("||", match));
+
+//int[] s = { 1, 2, 3, 3, 4};
+string[] q = match.Distinct().ToArray();
+
+System.Console.WriteLine(String.Join("||", q));
+
+
+for(byte i = 0; i < match.Length; i++)
+  {
+    byte count = 0;
+    for(byte j = 0; j < match.Length; j++) 
+      {
+        if(match[i] == match[j]) count++;
+        if(count > 1 && i >= j) break;
+        else if(count > 1)
+        {
+          System.Console.WriteLine(match[i]);
+          break;
+        }
+      }
+    if(count == 1) System.Console.WriteLine(match[i]);
+  }
+  
+    
---
+++ Seminar8/Task_59/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+byte[,] array2D = new byte[4, 4];
+
+for(var i = 0; i < array2D.GetLength(0); i++)
+  for(var j = 0; j < array2D.GetLength(1); j++)
+    array2D[i, j] = (byte)new Random().Next(10, 20);
+
+for(var i = 0; i < array2D.GetLength(0); i++)
+  {
+    for(var j = 0; j < array2D.GetLength(1); j++)
+      System.Console.Write(array2D[i, j] + " \\ ");
+    System.Console.WriteLine();
+  }
+
+var min = 0;
+for(var i = 0; i < array2D.GetLength(0); i++)
+  {
+    for(var j = 0; j < array2D.GetLength(1) - 1 ; j++)
+      if(array2D[i, j] < array2D[i, j + 1] && array2D[i, j] < min) min = array2D[i, j];
+      else if(array2D[i, j] < array2D[i, j + 1] && i == 0) min = array2D[i, j];
+  }
+
+System.Console.WriteLine(min);
